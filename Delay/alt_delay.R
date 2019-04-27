ab2f3319ca4361daf5b0333be9ad6b0d2f5ed672
samjrx <xujiar@oregonstate.edu>
2019-04-27 13:21:47 -0700

    Some polishings

--- Delay/alt_delay.R
+++ Delay/alt_delay.R
@@ -40,3 +40,20 @@ day_hour_sum %>%
   geom_tile(aes(fill = prop_over_15)) +
   scale_fill_gradient2(name = "Percent Difference", low="Red", mid="Yellow", high="Blue", midpoint=0, na.value="Black") +
   labs(x="Hour", y="Day", title="Difference in percentage of delays over 15 minutes of HOU to IAH")
+
+# Difference in proportion plot (Some polishing)
+day_hour_sum %>% 
+  group_by(DepHour, DayOfWeek) %>%
+  summarise(avg_delay = avg_delay[which(Origin == "HOU")][1]-avg_delay[which(Origin == "IAH")][1],
+            avg_delay_delayed = avg_delay_delayed[which(Origin == "HOU")][1]-avg_delay_delayed[which(Origin == "IAH")][1],
+            prop_over_15 = (prop_over_15[which(Origin == "HOU")][1]-prop_over_15[which(Origin == "IAH")][1])*100,
+  ) %>%
+  ggplot(aes(DepHour, DayOfWeek)) +
+  geom_tile(aes(fill = prop_over_15)) +
+  scale_fill_gradient2(name = "Percent Difference", low="Red", mid="Yellow", high="Blue", midpoint=0, na.value="white") +
+  labs(x="Hour", y="Day", 
+       title="How different can delays in IAH and HOU be ?",
+       subtitle = "Based on all departing flights from George Bush Intercontinental Airport (IAH) and 
+       William P. Hobby Airport (HOU) in 2011") + 
+  theme_classic() +
+  theme(axis.ticks.y = element_blank(), axis.line.y = element_blank()) + coord_equal()
